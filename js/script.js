29b2f7bcbeb46ed3d198bf3fb9ec8abab702d2dc
Yuheni Ziatsikau <user8791@mail.ru>
2022-02-18 23:00:23 +0300

    add new style and js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const sliderThumbs = new Swiper('.slider-thumbs', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    centeredSlides: true,
+    
+});
+
+sliderThumbs.on('click', (swiper) => {
+    swiper.slideTo(swiper.clickedIndex)
+})
+
+const sliderMain = new Swiper('.slider-main', {
+    loop: true,
+    thumbs: {
+        swiper: sliderThumbs,
+    },
+});
